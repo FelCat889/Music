c48622cddd72176e065cf49a092df4f4fc4623e7
Felix Cat <felixc0889@gmail.com>
2025-01-24 08:21:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ var colors = ["#8b99e1", "#8cc8a9", "#7ebae6", "#e0bb99", "#e0909e"];
 document.querySelectorAll("a").forEach(function(link){
     link.style.setProperty('--color', colors[Math.floor(Math.random() * colors.length)]);
 });
+
+//Wha happen
